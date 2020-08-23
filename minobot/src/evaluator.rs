83715e6a0c8a5231cf204c4935426bfa097d14bb
KSean222 <44050761+KSean222@users.noreply.github.com>
2020-08-23 11:03:16 +1000

    Evaluator cleanup

--- minobot/src/evaluator.rs
+++ minobot/src/evaluator.rs
@@ -13,14 +13,12 @@ pub struct StandardEvaluator {
     holes_sq: f64,
     hole_depths: f64,
     hole_depths_sq: f64,
-    move_height: f64,
-    move_height_sq: f64,
     filled_cells_x: f64,
     filled_cells_x_sq: f64,
     filled_cells_down: f64,
     filled_cells_down_sq: f64,
-    parity: f64,
-    parity_sq: f64,
+    move_height: f64,
+    move_height_sq: f64,
     max_height: f64,
     max_height_sq: f64,
     wells: f64,
@@ -43,14 +41,12 @@ impl Default for  StandardEvaluator {
             holes_sq: -7.0,
             hole_depths: -20.0,
             hole_depths_sq: -2.0,
-            move_height: -10.0,
-            move_height_sq: -0.5,
             filled_cells_x: 0.0,
             filled_cells_x_sq: 10.0,
             filled_cells_down: 0.0,
             filled_cells_down_sq: 10.0,
-            parity: 0.0,
-            parity_sq: -2.0,
+            move_height: -10.0,
+            move_height_sq: -0.5,
             max_height: -1.0,
             max_height_sq: -0.5,
             wells: -1.0,
@@ -90,8 +86,6 @@ impl Evaluator for StandardEvaluator {
         let mut max_height = 0;
         let mut wells = 0;
         let mut spikes = 0;
-        let mut even_cells = 0i32;
-        let mut odd_cells = 0i32;
         for x in 0..10 {
             let mut well_streak = 0;
             let mut spike_streak = 0;
@@ -111,11 +105,6 @@ impl Evaluator for StandardEvaluator {
                         }
                     }
                 } else {
-                    if (x + y) & 1 == 0 {
-                        even_cells += 1;
-                    } else {
-                        odd_cells += 1;
-                    }
                     if heights[x as usize] == 0 {
                         heights[x as usize] = height;
                         max_height = max_height.max(height);
@@ -132,6 +121,27 @@ impl Evaluator for StandardEvaluator {
             value += well_streak * self.well_depth;
             value += well_streak * well_streak * self.well_depth_sq;
         }
+        value += holes as f64 * self.holes;
+        value += (holes * holes) as f64 * self.holes_sq;
+        value += hole_depths as f64 * self.hole_depths;
+        value += hole_depths_sq as f64 * self.hole_depths_sq;
+        value += max_height as f64 * self.max_height;
+        value += (max_height * max_height) as f64 * self.max_height_sq;
+        value += wells as f64 * self.wells;
+        value += (wells * wells) as f64 * self.wells_sq;
+        value += (spikes as f64) * self.spikes;
+        value += (spikes * spikes) as f64 * self.spikes_sq;
+
+        let mut bumpiness = 0.0;
+        let mut bumpiness_sq = 0.0;
+        for (&h1, &h2) in heights.iter().zip(heights.iter().skip(1)) {
+            let diff = (h1 - h2).abs() as f64;
+            bumpiness += diff;
+            bumpiness_sq += diff * diff;
+        }
+        value += bumpiness * self.bumpiness;
+        value += bumpiness_sq * self.bumpiness_sq;
+
         let mut row_transitions = 0;
         for y in 20..40 {
             for x in 0..11 {
@@ -142,6 +152,7 @@ impl Evaluator for StandardEvaluator {
         }
         value += row_transitions as f64 * self.row_transitions;
         value += (row_transitions * row_transitions) as f64 * self.row_transitions_sq;
+
         let mut filled_cells_x = 0;
         let mut filled_cells_down = 0;
         for &(x, y) in &parent.board.current.cells(node.mv.r) {
@@ -157,36 +168,17 @@ impl Evaluator for StandardEvaluator {
                 filled_cells_down += 1;
             }
         }
-        let parity_diff = (even_cells - odd_cells).abs();
-        value += parity_diff as f64 * self.parity;
-        value += (parity_diff * parity_diff) as f64 * self.parity_sq;
         value += filled_cells_x as f64 * self.filled_cells_x;
         value += (filled_cells_x * filled_cells_x) as f64 * self.filled_cells_x_sq;
         value += filled_cells_down as f64 * self.filled_cells_down;
         value += (filled_cells_down * filled_cells_down) as f64 * self.filled_cells_down_sq;
+
         let move_height = 39 - node.mv.y;
         value += move_height as f64 * self.move_height;
         value += (move_height * move_height) as f64 * self.move_height_sq;
+
         reward += self.line_clear[node.lock.lines_cleared as usize];
-        value += (holes as f64) * self.holes;
-        value += ((holes * holes) as f64) * self.holes_sq;
-        value += (hole_depths as f64) * self.hole_depths;
-        value += (hole_depths_sq as f64) * self.hole_depths_sq;
-        value += (max_height as f64) * self.max_height;
-        value += ((max_height * max_height) as f64) * self.max_height_sq;
-        value += (wells as f64) * self.wells;
-        value += ((wells * wells) as f64) * self.wells_sq;
-        value += (spikes as f64) * self.spikes;
-        value += ((spikes * spikes) as f64) * self.spikes_sq;
-        let mut bumpiness = 0.0;
-        let mut bumpiness_sq = 0.0;
-        for (i, &h) in heights.iter().enumerate().skip(1) {
-            let diff = (h - heights[i]).abs() as f64;
-            bumpiness += diff;
-            bumpiness_sq += diff * diff;
-        }
-        value += bumpiness * self.bumpiness;
-        value += bumpiness_sq * self.bumpiness_sq;
+
         (value, reward)
     }
 }
